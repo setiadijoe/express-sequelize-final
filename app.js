e139570db2dbab8b2219bbd1125a0c491aa57eea
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-15 09:22:20 +0700

    sukses install

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const pg = require('pg');
+const Sequelize = require('sequelize')
+
+const index = require('./router/index')
+
+app.set('view engine', 'ejs')
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json())
+
+app.use('/',index)
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Hello I'm on 3000`);
+  })
